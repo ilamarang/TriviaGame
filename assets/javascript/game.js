65258e944da2670c71e633451d2e757ae1b18eb4
Ilamaran Gurunathan <ilamaran.g@gmail.com>
2017-04-24 20:46:09 -0400

    Updated skeleton with images and additional skin to the Quiz game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,15 @@ var questionCounter;
 //Define the list of Questions in an Object List that holds the quiz details.
 var correctAnswer;
 var userSelectedTopic;
-var userChoice= {};
+var userChoice = {};
 var currentQuizScore = 0;
 var overallQuizScore = 0;
 var totalQuizCount = 0;
 var currentUserChoice = "";
 var questionArray = [];
+var tickSymbol = "&#x2714";
+var crossSymbol = "&#x2717";
+
 var questionList = {
     "Music": {
         "property": {
@@ -17,36 +20,42 @@ var questionList = {
         },
 
         "Question1": {
-            question: "Music",
-            choiceGroup: ["2", "3", "4", "5"],
-            answer: "Hi",
+            question: "Identify the Album Cover",
+            choiceGroup: ["Rihanna - Rated R", "Micah Carey - Music Box", "Adele - 21", "Beyonce - I Am...Sasha Fierce"],
+            questionType: "pic",
+            questionFileLocation: "./assets/images/AdeleHidden.jpg",
+            answer: 2,
             answerDetails: 5,
             answerPicLocation: "Hi"
 
         },
 
         "Question2": {
-            question: "Question2",
-            
-            choiceGroup: ["Meow", "Bow", "Oink", "Neigh"],
-            answer: "Hi",
+            question: "Identify the Album Cover",
+            choiceGroup: ["Beastie Boys - Paul’s Boutique", "Bob Dylan - The Freewheelin’ Bob Dylan", "Pink Floyd - Wish You Were Here", "Oasis - (What’s the Story) Morning Glory"],
+            questionType: "pic",
+            questionFileLocation: "./assets/images/OasisHidden.jpg",
+            answer: 3,
             answerDetails: 5,
             answerPicLocation: "Hi"
         },
 
         "Question3": {
-            question: "Question3",
-            
-            choiceGroup: ["-15", "-90", "-20", "-40"],
-            answer: "Hi",
+            question: "Identify the Music Band",
+            choiceGroup: ["Grateful Dead", "The Who", "The Allman Brothers", "Eagles"],
+            questionType: "pic",
+            questionFileLocation: "./assets/images/AllmanBrothersHidden.jpg",
+            answer: 2,
             answerDetails: 5,
             answerPicLocation: "Hi"
         },
 
         "Question4": {
-            question: "Question4",
-            choiceGroup: ["Hi", "How", "Yes", "Go"],
-            answer: "Hi",
+            question: "Identify the Music Band",
+            choiceGroup: ["Led Zipperlin", "Aerosmith", "Eagles", "Pink Floyd"],
+            questionType: "pic",
+            questionFileLocation: "./assets/images/LedZipperlin.jpg",
+            answer: 0,
             answerDetails: 5,
             answerPicLocation: "Hi"
         }
@@ -68,7 +77,7 @@ var questionList = {
 
         "Question2": {
             question: " 'We The People' opening of which founding document?",
-            
+
             choiceGroup: ["The Declaration of Independence", "The Bill of Rights", "The Constitution", "Obamacare"],
             answer: 2,
             answerDetails: 5,
@@ -77,7 +86,7 @@ var questionList = {
 
         "Question3": {
             question: "How many Supreme Court justices are there?",
-            
+
             choiceGroup: ["8", "9", "11", "12"],
             answer: 2,
             answerDetails: 5,
@@ -110,7 +119,7 @@ var questionList = {
 
         "Question2": {
             question: "Question2",
-            
+
             choiceGroup: ["Meow", "Bow", "Oink", "Neigh"],
             answer: "Hi",
             answerDetails: 5,
@@ -119,7 +128,7 @@ var questionList = {
 
         "Question3": {
             question: "Question3",
-            
+
             choiceGroup: ["-15", "-90", "-20", "-40"],
             answer: "Hi",
             answerDetails: 5,
@@ -143,7 +152,7 @@ var questionList = {
 
         "Question1": {
             question: "Sports",
-            
+
             choiceGroup: ["2", "3", "4", "5"],
             answer: "Hi",
             answerDetails: 5,
@@ -187,11 +196,12 @@ var initialize = function() {
     arrayCount = 0;
     currentUserChoice = "";
     currentQuizScore = 0
-    $("#timerPanel").hide();    
+    $("#timerPanel").hide();
     $("#nextButton").hide();
     $("#restartButton").hide();
     $("#questionPanel h2").hide();
     $(".answerPanel").hide();
+
 };
 
 //Dynamically display players based on the object List.
@@ -211,94 +221,110 @@ var displayQuizTopics = function(quiz) {
     $captionDiv.appendTo($columnDisplay);
     $columnDisplay.appendTo($("#quizTopics"));
 
+
 }
 
 var displayNextQuestion = function() {
 
     $("#questionPanel").hide();
     $(".answerPanel").hide();
+    $(".questionPic").empty();
+    $(".answerResultPic").empty();
+    currentUserChoice = "";
     userChoice = questionList[userSelectedTopic];
-    
-    $("#questionPanel h3").html(userChoice[questionArray[questionCounter]].question);
+
+    $("#questionPanel h3").html(userChoice[questionArray[questionCounter]].question );
+    if (userChoice[questionArray[questionCounter]].questionType == "pic") {
+        var $questionImage = $("<img class='img-responsive' width='100%'>").attr("src", userChoice[questionArray[questionCounter]].questionFileLocation);
+        $questionImage.appendTo($(".questionPic"));
+    }
+
     for (i = 0; i < userChoice[questionArray[questionCounter]].choiceGroup.length; i++) {
 
-        var radioChoice = '<div class="radio"> <label> <input type="radio" name="answer" value="' + userChoice[questionArray[questionCounter]].choiceGroup[i] + '" >' + userChoice[questionArray[questionCounter]].choiceGroup[i] + '</label>';
+        if (userChoice[questionArray[questionCounter]].answer===i) {
+            var radioChoice = '<div class="radio"> <label> <input type="radio" name="answer" value="' + userChoice[questionArray[questionCounter]].choiceGroup[i] + '" >' + userChoice[questionArray[questionCounter]].choiceGroup[i] + "<span id='symbol'>" + '</label>';
+        }else {
+            var radioChoice = '<div class="radio"> <label> <input type="radio" name="answer" value="' + userChoice[questionArray[questionCounter]].choiceGroup[i] + '" >' + userChoice[questionArray[questionCounter]].choiceGroup[i] + "<span id='symbol'>" + '</label>';
+        }
         
+
         $("#choiceList").append(radioChoice).fadeIn;
     }
     questionCounter++;
+
 
     $("#questionPanel").animate({
         "opacity": "show",
         top: "150"
     }, 1000);
 
+
+
 };
 
 
 
 var verifyAnswer = function() {
-       
-  console.log(currentUserChoice);
-  console.log(userChoice[questionArray[questionCounter - 1]]);
 
-  if (userChoice[questionArray[questionCounter - 1]].choiceGroup[userChoice[questionArray[questionCounter - 1]].answer] === currentUserChoice) 
-  {
-    currentQuizScore++;
-  }
-   else
-   {
-    console.log("Better Luck Next Time");
-   }
-       
+    console.log("currentUserChoice" + currentUserChoice);
+
+    console.log(userChoice[questionArray[questionCounter - 1]].choiceGroup[userChoice[questionArray[questionCounter - 1]].answer]);
+
+
+    if (userChoice[questionArray[questionCounter - 1]].choiceGroup[userChoice[questionArray[questionCounter - 1]].answer] === currentUserChoice) {
+        currentQuizScore++;
+
+        
+
+    }
+
 
 
 }
 
 var handleTimeOut = function() {
-    console.log("Timer Timed out Event")
+
 
     $('#nextButton').trigger('click');
 
 }
-    
+
 
 $(document).ready(function() {
 
 
     $("#restartButton").on("click", function() {
-      
-      initialize();
-      $("#quizTopics").show();  
+
+        initialize();
+        $("#quizTopics").show();
     });
 
 
-    
 
 
     initialize();
 
     for (var key in questionList) {
-        
+
         if (questionList.hasOwnProperty(key)) {
             displayQuizTopics(questionList[key]);
         }
     }
 
-    
+
 
     $(".img-responsive").on("click", function() {
 
         userSelectedTopic = $(this).attr("id");
         for (var key in questionList[userSelectedTopic]) {
             if (key != "property") {
-                
+
                 questionArray[arrayCount] = key;
-                console.log(questionArray);
+
                 arrayCount++;
             }
 
-                    }
+        }
         $("#quizTopics").hide();
         //$("#nextButton").show();
 
@@ -306,28 +332,48 @@ $(document).ready(function() {
         totalQuizCount++;
     });
 
-    $("#choiceList").on("click", "input", function(){
+    $("#choiceList").on("click", "input", function() {
+        $(":radio").prop("disabled", true);
+        
+        if (currentUserChoice === "") {
+            currentUserChoice = $(this)[0].value;
+            
+            $(".answerPanel").show();
+            var randomAnswerGif = Math.floor((Math.random() * 10) + 1);
+            var source = "";
+            if (userChoice[questionArray[questionCounter - 1]].choiceGroup[userChoice[questionArray[questionCounter - 1]].answer] === currentUserChoice) {
+                source = "./assets/gifs/correctAnswer" + randomAnswerGif + ".gif"
+                $(this).next().html(tickSymbol);
+                //console.log($(this));                  
 
-    	currentUserChoice = $(this)[0].value; 
 
-        $(".answerPanel").show();  	
-        	
+            } else {
+                source = "./assets/gifs/wrongAnswer" + randomAnswerGif + ".gif"
+                $(this).next().html(crossSymbol);
+
+            }
+            var $answerCorrectImage = $("<img class='img-responsive' width='100%'>").attr("src", source);
+            $answerCorrectImage.appendTo($(".answerResultPic"));
+            
+        }
+
+
     });
 
-    
 
-$("#nextButton").on("click", function() {
 
-        if(currentUserChoice.length > 0) 
-        {
-            verifyAnswer(); 
-                   
+    $("#nextButton").on("click", function() {
+
+        if (currentUserChoice.length > 0) {
+
+            verifyAnswer();
+
         }
-        
 
-        
+
+
         CounterFreeze();
-        CounterInit(10);
+        CounterInit(5);
 
         $("#timerPanel").hide();
         //Delete Previous Question
@@ -341,9 +387,11 @@ $("#nextButton").on("click", function() {
             }, 1000);
 
         } else {
+            $(".questionPic").empty();
+            $(".answerPanel").hide();
             $("#questionPanel h3").html("Thanks for Playing!");
             overallQuizScore = overallQuizScore + currentQuizScore;
-            $("#questionPanel h2").html("Results  <br> " + "Current Quiz Score " + currentQuizScore + "/4  <br>" + "Total Quiz Attempted: " + totalQuizCount + "<br> Overall Quiz Score: " + overallQuizScore + "/" + 4*totalQuizCount);
+            $("#questionPanel h2").html("Results  <br> " + "Current Quiz Score " + currentQuizScore + "/4  <br>" + "Total Quiz Attempted: " + totalQuizCount + "<br> Overall Quiz Score: " + overallQuizScore + "/" + 4 * totalQuizCount);
             $("#questionPanel h2").show();
 
             CounterFreeze();
@@ -357,5 +405,39 @@ $("#nextButton").on("click", function() {
 
 
     });
+
+    $(function () {
+    var body = $(".panel");
+    var backgrounds = [
+      'url(./assets/images/1_4.jpg)', 
+      'url(./assets/images/1_2.jpg)', 
+      'url(./assets/images/1_3.jpg)', 
+      'url(./assets/images/1_4.jpg)', 
+      'url(./assets/images/2.jpg)', 
+      'url(./assets/images/2_2.jpg)', 
+      'url(./assets/images/3.jpg)', 
+      'url(./assets/images/3_2.jpg)', 
+      'url(./assets/images/4.jpg)', 
+      'url(./assets/images/4_2.jpg)', 
+      'url(./assets/images/5.jpg)', 
+      'url(./assets/images/5_2.jpg)', 
+      'url(./assets/images/6.jpg)', 
+      'url(./assets/images/6_2.jpg)', 
+      'url(./assets/images/7.jpg)', 
+      'url(./assets/images/7_2.jpg)'
+
+      ];
+    var current = 0;
+
+    function nextBackground() {
+        body.css(
+            'background',
+        backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    body.css('background', backgrounds[0]);
+});
 
 });
